13b721a2733cb19550c5a4465ced3e73cb5afe6e
Mani Movassagh <mani.mgh@gmail.com>
2024-04-19 12:01:43 +0200

    test

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,6 +1,6 @@
 return{
     {'akinsho/toggleterm.nvim', version = "*", opts = {
-      open_mapping = [[<leader>t]],
+      open_mapping = [[<leader>te]],
       direction = "float",
       shade_terminals = true
     }
